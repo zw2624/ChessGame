fe80fff686ca69e6b49b1ff1ba85bda9a9d66088
zwang199 <zwang199@illinois.edu>
2019-02-01 16:18:53 -0600

    ,,

--- src/Main/Pieces/Knight.java
+++ src/Main/Pieces/Knight.java
@@ -4,8 +4,12 @@ import Main.Board;
 import Main.Piece;
 
 public class Knight extends Piece {
+
     @Override
     public boolean checkMove(Board grid, int fromX, int fromY, int toX, int toY) {
-        return false;
+        boolean notSameLine = (fromX != toX) & (fromY != toY);
+        boolean hasMine = grid.getCell(toX, toY).getOwner() == grid.getCell(fromX, fromY).getOwner();
+        boolean isThree = (Math.abs(fromX - toX) + Math.abs(fromY - toY)) == 3;
+        return notSameLine & (!hasMine) & isThree;
     }
 }
